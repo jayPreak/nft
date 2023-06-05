9d80f7ba6cd37df8ee0d6049ec5b9d2814f852fa
jayPreak <jayesh.preak2003@gmail.com>
2023-06-05 16:16:20 +0530

    nft 🤓

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+// "https://rpc-mumbai.maticvigil.com"
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
